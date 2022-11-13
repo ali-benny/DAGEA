e35c2da2032a09aabc06dd8cc5aebdd277357b5d
CampanelliEmanuele <emanuele.campanelli@studio.unibo.it>
2022-11-13 19:14:42 +0100

    Chore: better GUI

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ let searchBy = (searchCase) => {
     var optionsButton = document.getElementById("tweetSearchBarButton")
     switch (searchCase) {
         case 0:
-            optionsButton.innerHTML = "Research by user"
+            optionsButton.innerHTML = "Research by user "
             break;
         case 1:
-            optionsButton.innerHTML = "Research by tag"
+            optionsButton.innerHTML = "Research by tag "
             break;
         default:
             break;
