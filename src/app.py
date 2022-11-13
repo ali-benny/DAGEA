6d8f0ccc633fddaea7e6080150cadb40fc512417
Alice Benatti <alice17bee@gmail.com>
2022-11-13 14:46:06 +0100

    🖥️ wip: add getTweet and generate db automatic
    non è ancora tutto funzionante: manca collegare il campo di ricerca, farsi stampare il db su html etc

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
-import helloWorld
+import twitter 
+import sqlite3
+
 app = Flask(__name__)
 
 @app.route('/')
---
+++ src/getTweet.py
@@ -0,0 +1,43 @@
+import twitter
+import sqlite3
+
+def convertDF2SQL(function, maxTweet, findParam):
+	"""
+	The convertDF2SQL function takes in a function, maxTweet and findParam. 
+	In base alla funzione che voglio usare per cercare tweet,
+	ricavo il dataframe e lo salvo in un file database.db
+	
+	:param function: Specify which function to use
+	:param maxTweet: Limit the number of tweets that will be downloaded
+	:param findParam: param to search by de function
+	:return: database.db file otherwise send Error if no correct parameter
+	
+	..notes: https://www.youtube.com/watch?v=hDNxHiybF8Q
+	https://datatofish.com/pandas-dataframe-to-sql/
+	"""
+	if function == "GetTweetByUser":
+		user = findParam
+		df = twitter.GetTweetByUser(user, maxTweet)
+	elif function == "GetTweetByKeyword":
+		keyword = findParam
+		df = twitter.GetTweetByKeyword(keyword, maxTweet)
+	else: 
+		df = ""
+		return "ERROR: please insert correct parameter"
+	
+	# -- create db --
+	connection = sqlite3.connect('database.db')
+	c = connection.cursor()
+		# - create table -
+	c.execute('CREATE TABLE IF NOT EXISTS Tweet (user, desc)')
+	connection.commit()	# save my edits on connection
+
+	# -- insert data on data_frame into db connection
+	df.to_sql('Tweet', connection, if_exists='replace', index = True)
+
+def __main__():
+	twitter.__init__()
+	convertDF2SQL('GetTweetByKeyword', 200, 'prova')
+
+if __name__ == '__main__':
+	__main__()
