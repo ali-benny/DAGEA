7362984eb160b25bb20fab422e5471cca0b18d9a
Gabriele Buttinoni <gabriele.buttinoni@gmail.com>
2022-11-15 16:57:25 +0100

    Worka il bottone

--- src/app.py
+++ src/app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
-import getTweet 
+from flask import Flask, render_template, request, redirect
+import getTweet
 import sqlite3
 import jinja2
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=('PUT', 'GET', 'POST', 'DELETE', 'PATCH'))
 def homepage():
+	if request.method == 'POST':
+		keyword = request.form['keyword']		# getting keyword from form
+		getTweet.convertDF2SQL('GetTweetByKeyword', 175, keyword)
+		return redirect('/')
 	getTweet.__main__()		# getTweet and save them into db
 	connection = sqlite3.connect('database.db')		# connecting to database
 	connection.row_factory = sqlite3.Row	# read row from database
