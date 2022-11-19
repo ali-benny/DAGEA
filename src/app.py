065deccd1cfabce06df3db07c8e2781c79d3aebc
CampanelliEmanuele <emanuele.campanelli@studio.unibo.it>
2022-11-19 19:18:01 +0100

    Bump: tweetLimit value is saved

--- src/app.py
+++ src/app.py
@@ -16,7 +16,7 @@ def homepage():
 		connection.row_factory = sqlite3.Row	# read row from database
 		tweets = connection.execute('SELECT * FROM tweet').fetchall() 	# getting all tweet form db
 		connection.close()	# close connection to database
-		return render_template('index.html', tweets=tweets)		# rendering flask template 'index.html'
+		return render_template('index.html', tweets=tweets, tweetLimit=tweetLimit)		# rendering flask template 'index.html'
 	getTweet.__main__()		# getTweet and save them into db
 	connection = sqlite3.connect('database.db')		# connecting to database
 	connection.row_factory = sqlite3.Row	# read row from database
--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,4 +1,5 @@
-var cd = new Date();        /* cd stay for Current Date */
+let cd = new Date();        /* cd stay for Current Date */
+let tweetLimit = 6;         /* The default value of the tweetLimit input */
 
 /*  Funzione (momentaneamente) inutilizzata. Potrebbe tornare utile in futuro ergo teniamola ancora un po'
 let searchBy = (searchCase) => {
@@ -15,6 +16,15 @@ let searchBy = (searchCase) => {
     }
 }*/
 
+let initializeTweetLimit = (tweetLimitInput) => {
+    /**
+     * Questa funziona setta a <tweetLimit> il numero minimo di tweet da ricercare
+     * qualora il campo dell'input sia vuoto (ovvero al load della home page)
+     */
+    if (tweetLimitInput.getAttribute('value') == "")
+        tweetLimitInput.setAttribute('value', tweetLimit)
+}
+
 let initializeDate = (dateElement, isStartDate) => {
     var cYear = cd.getFullYear()
     var cMonth = String(cd.getMonth() + 1)
@@ -27,5 +37,6 @@ let initializeDate = (dateElement, isStartDate) => {
 
 initializeDate(document.getElementById("startDate"), true);
 initializeDate(document.getElementById("limitDate"), false);
+initializeTweetLimit(document.getElementById("tweetLimitInput"));
 
 console.log("End of Index.js")      /* A print to see if all the index.js code ran correctly */
