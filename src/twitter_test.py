e243811cb6cefa7cfd2844d01d101cfa52f6844b
alicealtena <alice.altena@gmail.com>
2022-11-23 16:31:36 +0100

    init test done (a little bit)

--- src/twitter_test.py
+++ src/twitter_test.py
@@ -1,25 +1,29 @@
 import unittest
 import pandas as pd
 import configparser
-from twitter import get_key #, __init__
+from tweepy import api, OAuthHandler
+from twitter import get_key, __init__
 
 class TestGetKey(unittest.TestCase):
 
-    def test_get_key(self):
+    def setUp(self):
         # da sostituire con i propri 'section' e 'setting' in tutti i campi dove c'è get_key
-        api_key = get_key('twitter', 'api_key')
-        self.assertEqual(api_key, 'api_key value') # inserire nel secondo campo la propria api key
+        self.api_key = get_key('twitter', 'api_key')
+        self.api_key_secret = get_key('twitter', 'api_key_secret')
+        self.access_token = get_key('twitter', 'access_token')
+        self.access_token_secret = get_key('twitter', 'access_token_secret')
+       
+    def test_get_key(self):
+        self.assertEqual(self.api_key, 'api_key value') # inserire nel secondo campo la propria api key
+        self.assertEqual(self.api_key_secret, 'api_key_secret value') # inserire nel secondo campo la propria api key secret
+        self.assertEqual(self.access_token, 'access_token value') # inserire nel secondo campo il proprio access token
+        self.assertEqual(self.access_token_secret, 'access_token_secret value') # inserire nel secondo campo il proprio access token secret
 
-        api_key_secret = get_key('twitter', 'api_key_secret')
-        self.assertEqual(api_key_secret, 'api_key_secret value') # inserire nel secondo campo la propria api key secret
-
-        access_token = get_key('twitter', 'access_token')
-        self.assertEqual(access_token, 'access_token value') # inserire nel secondo campo il proprio access token
-
-        access_token_secret = get_key('twitter', 'access_token_secret')
-        self.assertEqual(access_token_secret, 'access_token_secret value') # inserire nel secondo campo il proprio access token secret
-
-        # self.assertRaises(configparser.NoOptionError)
+        # self.assertRaises(configparser.NoOptionError, get_key, 'twitter', None)
+    
+    def test__init__(self):
+        if not self.api_key or not self.api_key_secret:
+            self.fail("Wrong api key and/or secret")
 
 """
 class TestGetTweet(unittest.TestCase):
