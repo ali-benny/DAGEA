338574ae57d9430dfc475edbb8905c7b6db6b981
CampanelliEmanuele <emanuele.campanelli@studio.unibo.it>
2022-12-05 02:05:48 +0100

    Bump: general improvements

--- src/TweetSearch.py
+++ src/TweetSearch.py
@@ -1,18 +1,20 @@
 import os
+from os import listdir
 try:
-    import tweepy
-    import sqlite3
-    import pandas as pd
-    import configparser
+    import tweepy                  # Used for APIs
+    import pandas as pd         # Used for data handling and debug
+    import configparser         # Used for APIv1 initialization
+    import csv                        # Used for APIv2
+    import json                      # Used for APIv2
 except ModuleNotFoundError:
     os.system('pip install tweepy')
-    os.system('pip install sqlite3')
-    os.system('pip install config')
     os.system('pip install pandas')
     os.system('pip install configparser')
+    os.system('pip install csv')
+    os.system('pip install json')
 
 class APIv1:
-    ################ API SETUP ################
+    ################################ API SETUP ################################
     section = 'twitter'		#! change it as your [param] on config.ini file 
     APILabel = 'APILabel'
 
@@ -37,11 +39,10 @@ class APIv1:
     def setAPILabel(cls, APILabel) -> None:
         cls.APILabel = APILabel
     
-    ################  RESEARCH  ################
+    ################################  RESEARCH  ################################
     @classmethod
     def getCoordinates(cls, tweet_id, client):
         tweetInfo = client.get_tweet(tweet_id, expansions=['geo.place_id'])
-        pd.DataFrame(tweetInfo.data).to_csv('tweetInfoResponseData.csv')
         try:
             place_id = tweetInfo.data.geo['place_id']
             placeObj = cls.api.geo_id(place_id)
@@ -53,7 +54,7 @@ class APIv1:
             return [0.0,0.0]
         
 class APIv2:
-    ################ API SETUP ################
+    ################################ API SETUP ################################
     @classmethod
     def __init__(cls) -> None:
         # TODO: Trovare un modo migliore per esportare questo token di mrd
@@ -64,7 +65,7 @@ class APIv2:
     def __init__response(cls) -> None:
         cls.response = cls.client.get_user(id=0)    # Questa chiamata di get_user ritornera' un dato response vuoto (analogo ad una string avuota '')
 
-    ################  ATTRIBUTE SETTING   ################
+    ################################  ATTRIBUTE SETTING   ################################
     query = ''
     tweetsLimit = 10
     start_time = ''
@@ -83,16 +84,16 @@ class APIv2:
         if end_time is not None:
             cls.end_time = end_time
 
-    ################  RESEARCH  ################
+    ################################  RESEARCH  ################################
     @classmethod
     def researchDecree(cls, researchType: str) -> None:
         match researchType:
             case 'researchByUser':
                 cls.getTweetByUser(username=cls.query)
             case 'researchByKeyword':
-                cls.getTweetByKeyword(query=cls.query, tweetsLimit=cls.tweetsLimit, start_time=cls.start_time, end_time=cls.end_time, expansions=cls.expansions)
+                cls.getTweetByKeyword(query=cls.query, tweetsLimit=cls.tweetsLimit, start_time=cls.start_time, end_time=cls.end_time)
             case 'researchByHashtag':
-                cls.getTweetByKeyword(query='#'+cls.query, tweetsLimit=cls.tweetsLimit, start_time=cls.start_time, end_time=cls.end_time, expansions=cls.expansions)
+                cls.getTweetByKeyword(query='#'+cls.query, tweetsLimit=cls.tweetsLimit, start_time=cls.start_time, end_time=cls.end_time)
             case _:
                 raise ValueError("ERROR: APIv2 Class, researchDecree: match error")
  
@@ -104,53 +105,55 @@ class APIv2:
             cls.response = cls.client.get_users_tweets(id=userId)
 
     @classmethod
-    def getTweetByKeyword(cls, query: str, tweetsLimit=None, start_time=None, end_time=None, expansions=None) -> None:
+    def getTweetByKeyword(cls, query: str, tweetsLimit=None, start_time=None, end_time=None) -> None:
         #cls.response = cls.client.search_recent_tweets(query=query, max_results=tweetsLimit, start_time=start_time, end_time=end_time)
-        cls.response = cls.client.search_recent_tweets(query=query, max_results=tweetsLimit, expansions=expansions)
+        cls.response = cls.client.search_recent_tweets(query=query, max_results=tweetsLimit, expansions=cls.expansions)
 
     @classmethod
     def createCard(cls):
         if cls.response.data is not None:
             card=[]
+            APIv1.__init__()
             for tweet in cls.response.data:
-                APIv1.__init__()
                 coordinates = APIv1.getCoordinates(tweet_id=tweet.id, client=cls.client)
                 text = tweet.text
                 tmp = cls.client.get_user(id=tweet.author_id).data
                 username = tmp if tmp is not None else 'Unknown'
                 card.append({"username": username, "text": text, "coordinates": coordinates})
-                #print(f"CARD:\n{card}")
             return card
         else:
             return ''
 
-    ################  DEBUG  ################
+    ################################  DEBUG  ################################
     @classmethod
-    def _createDataFrames(cls, response, field: str = 'all') -> None:
+    def _createDataFrames(cls) -> None:
         cls.dataFrames = []
-        match field:
-            case 'all':
-                cls.dataFrames.append(pd.DataFrame(response))
-                cls.dataFrames.append(pd.DataFrame(response.data))
-                cls.dataFrames.append(pd.DataFrame(response.includes))
-                cls.dataFrames.append(pd.DataFrame(response.errors))
-            case 'response':
-                cls.dataFrames.append(pd.DataFrame(response))
-            case 'data':
-                cls.dataFrames.append(pd.DataFrame(response.data))
-            case 'includes':
-                cls.dataFrames.append(pd.DataFrame(response.includes))
-            case 'errors':
-                cls.dataFrames.append(pd.DataFrame(response.errors))
-            case _:
-                raise ValueError("ERROR: APIv2 Class, createDataFrames: match error")
-        
+        cls.dataFrames.append(pd.DataFrame(cls.response))
+        cls.dataFrames.append(pd.DataFrame(cls.response.data))
+        try:
+            cls.dataFrames.append(pd.DataFrame(cls.response.includes))
+        except ValueError:      # Caso in cui i campi di response,includedes hanno lunghezze diverse (a causa della presenza di piu' valori nel campo extensions)
+            pass
+        cls.dataFrames.append(pd.DataFrame(cls.response.errors))
+    
     @classmethod
     def _createCsvFile(cls) -> None:
+        cls._deleteCsvFile()             # Si eliminano i .csv di ricerche passate
+        cls._createDataFrames()     # E si creano quelli coi nuovi dati
         try:
             cls.dataFrames[0].to_csv('response.csv')
             cls.dataFrames[1].to_csv('responseData.csv')
-            cls.dataFrames[2].to_csv('responseIncludes.csv')
-            cls.dataFrames[3].to_csv('responseErrors.csv')
+            try:
+                cls.dataFrames[2].to_csv('responseIncludes.csv')
+                cls.dataFrames[3].to_csv('responseErrors.csv')
+            except IndexError:
+                cls.dataFrames[2].to_csv('responseErrors.csv')
         except AttributeError:      # Caso in cui dataFrames[i] sia vuoto (non si sono chiamati i metodi del'API)
             print("ATTENTION: currently there is no 'cls.dataFrames[i]' attribute in the APIv2 class.\nPossible causes:\n1) You haven't called any APIv2 method of the class yet --> Call one\n2) The call returned no results --> Try modifying the query")
+
+    @classmethod
+    def _deleteCsvFile(cll)-> None:
+        myPath = os.path.dirname(os.path.abspath(__file__)) + '/'
+        for file in listdir(myPath):
+            if file.endswith('.csv'):
+                os.remove(myPath + file)
