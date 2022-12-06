7a6324b58d3a017df260ed19337764c97cc82726
CampanelliEmanuele <emanuele.campanelli@studio.unibo.it>
2022-12-06 19:47:19 +0100

    Fix: searchBykey without data range works again

--- src/utils.py
+++ src/utils.py
@@ -5,10 +5,10 @@ from datetime import timedelta
 # File creato al fine di avere un app.py piu' pulito e senza codice riguardante il frontend
 
 def initializeDates():
-    currentDate = datetime.datetime.now()
+    currentDate = datetime.datetime.now() - timedelta(days=1)	# To avoid this problem: https://stackoverflow.com/questions/66728533/twitter-api-v2-response-failing-when-params-has-start-time-end-time
     tmp = str(currentDate)
     maxDate = tmp[0:10] + 'T' + tmp[11:16]
-    tmp = str(currentDate - timedelta(days=7))
+    tmp = str(currentDate - timedelta(days=6))
     minDate = tmp[0:10] + 'T' + tmp[11:16]
     return {"minDate":minDate, "minDateValue": minDate, "maxDate": maxDate, "maxDateValue": maxDate}		# Returns a dictionary
 
