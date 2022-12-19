0baee75f7de6d45d5814bd3ea00e180bf1b0de83
Emanuele Campanelli <emanuele.campanelli@studio.unibo.it>
2022-12-19 17:33:45 +0000

    Marks merge in main

--- src/static/js/map.js
+++ src/static/js/map.js
@@ -1,33 +1,54 @@
 console.log("🧪map.js loaded");
 const map = L.map("map").setView([44.491026, 11.335586], 1);
-console.log("🚀");
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 	maxZoom: 19,
 	attribution:
 		'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
-console.log("👍");
-// connecting to database
-exports.select = function (callback){
-	
-console.log("🟨");
-    var db = new sqlite3.Database("../../database.db");           
-    db.all("SELECT * FROM all_tweets", function(err,rows){
-         if(err) return callback(err);
-         rows.forEach(function (row) { 
-			if (row.location != null) {
-				addMarker(row.location, row.tweet, row.user);
+console.log("before");
+
+console.log('inside')
+var db = new sqlite3.Database("../../marks.db");
+var db = new sqlite3.Database("../../topiBastardi.db");
+db.all("SELECT * FROM marks", function(err,rows) {
+		if (err) return callback(err);
+		rows.forEach(function (row) { 
+			if (!(row.lat == 0.0 && row.lon == 0.0)) {
+				//addMarker(row.lat, row.lon, row.user, row.text);
+				addMarker(row.lat, row.lon, 'row.user', 'row.text');
 			}
 		});
-        db.close();
-        return callback(null);
+	db.close();
+	return callback(null);
+}); 
+
+/*
+exports.select =function (callback) {
+	console.log('inside')
+	var db = new sqlite3.Database("../../marks.db");
+	var db = new sqlite3.Database("../../topiBastardi.db");
+	db.all("SELECT * FROM marks", function(err,rows) {
+			if (err) return callback(err);
+			rows.forEach(function (row) { 
+				if (!(row.lat == 0.0 && row.lon == 0.0)) {
+					//addMarker(row.lat, row.lon, row.user, row.text);
+					addMarker(row.lat, row.lon, 'row.user', 'row.text');
+				}
+			});
+		db.close();
+		return callback(null);
 	}); 
 }
-
-function addMarker(coordinate, tweet, user) {
-	const marker = L.marker(coordinate).addTo(map);
+*/
+function addMarker(lat, lon, user, tweet) {
+	const marker = L.marker([lat, lon]).addTo(map);
 	marker.bindPopup("<b>" + user + "</b><br>" + tweet);
+
+	// var c1 = 44.491026; var c2 = 11.335586
+	//tmp = L.marker([c1, c2]).addTo(map)
+	//tmp.bindPopup("<b>" + 'Nome Utente 1' + "</b><br>" + 'Testo del tweet 1');
 }
+
 // for (const row in tweets) {
 // 	marker.append(L.marker([tweet.lat, tweet.lon]).addTo(map).bindPopup(tweet.tweet));
 // }
