fc95c9e4a75567fab824ea45a69ffe5313ec94e0
CampanelliEmanuele <emanuele.campanelli@studio.unibo.it>
2023-01-06 18:05:19 +0100

    Fix&Cut: homepage research works again&unused code

--- src/app.py
+++ src/app.py
@@ -85,44 +85,54 @@ def method_post(request):
 
 @app.route('/', methods=('GET', 'POST'))
 def homepage():
-	global currentResearchMethod, dates, query, tweetsLimit, start_time, end_time, renderfilename, whatBtn
+	tweets = []  # list of tweets
+	query = ''
+	currentResearchMethod = ''
+	mapVisibility = 'hidden'
 	if request.method == 'POST':
-		renderfilename = 'index.html'
-		method_post(request)
-	return render_template('index.html', 
-		researchMethods=researchMethods,
-		currentResearchMethod=currentResearchMethod,
-		dates=utils.initializeDates('HTMLFormat'),
-		tweetsLimit=ts.APIv2.tweetsLimit,
-		mapVisibility='hidden'
-	)
-
-@app.route('/search', methods=('GET', 'POST'))
-def search():						# the currently chosen search method
-	global currentResearchMethod, dates, query, tweetsLimit, start_time, end_time, renderfilename, is_stream
-	is_stream = False
-	renderfilename = 'index.html'
-	if request.method == 'POST':	
-		method_post(request)			# set the current research method
+		#renderfilename = 'index.html'
+		#method_post(request)whatBtn = request.form['btnradio']
+		whatBtn = request.form['btnradio']
+		tweetsLimit = request.form['tweetsLimit']
+		query = request.form['keyword']
+		currentResearchMethod = request.form.get('researchBy')
+		dates['minDateValue']=request.form['minDate']
+		dates['maxDateValue']=request.form['maxDate']
+		mapVisibility = 'hidden'
+		if whatBtn == 'Stream':
+			is_stream = True
+			# getting stream tweets
+			stream.StreamByKeyword(query, (int)(tweetsLimit))
+			tweets = stream.MyStream.tweets
+		elif whatBtn == 'Search':
+			# getting tweets from twitter API
+			ts.APIv2.setDatas(query=query, tweetsLimit=tweetsLimit, start_time=dates['minDateValue'], end_time=dates['maxDateValue'])
+			ts.APIv2.researchDecree(researchType = currentResearchMethod)
+			tweets = ts.APIv2.createCard()
+			if ts.APIv2.hasCardsGeo(tweets):
+				mapVisibility = 'visible'
+				m.Map.__init__()
+				m.Map.addMarkers(tweets)
+		else:
+			print('Error: unknown button')
+		return render_template(renderfilename, 
+			tweetCards=tweets,
+			keyword = query,
+			tweetsLimit = 10,
+			researchMethods=researchMethods,
+			currentResearchMethod=currentResearchMethod,
+			dates=dates,
+			mapVisibility=mapVisibility
+		)
 	
-	if is_stream:
-		# getting stream tweets
-		tweets = stream.MyStream.tweets
-	else:
-		# Reinizizalizzazione col fine di avere di avere un reset dei campi quanto si torna in index
-		ts.APIv2._APIv2__init__response()
-		# tweets = ts.APIv2.createCard()
-		# m.Map.addMarkers(tweets)	# Vengono aggiunti i mark per ogni coordinata trovata
-
-	dates = utils.initializeDates('HTMLFormat')
-	return render_template(
-		renderfilename,
-		# tweetCards=tweets,
-		tweetsLimit=ts.APIv2.tweetsLimit,
+	return render_template('index.html', 
+		tweetCards=tweets,
+		keyword = query,
+		tweetsLimit = 10,
 		researchMethods=researchMethods,
 		currentResearchMethod=currentResearchMethod,
 		dates=dates,
-		# mapVisibility = 'hidden'
+		mapVisibility=mapVisibility
 	)
 
 @app.route('/eredita', methods=('GET', 'POST'))
